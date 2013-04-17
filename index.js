47cba4b70aec9b069d3da08b255f80b996fcacfa
Dave Dopson <ddopson@gmail.com>
2013-04-17 13:43:25 -0700

    comment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+
+// The 'bindings' helper will try all the well-known paths that the module might compile to based on platform / version / debug / etc
 module.exports = require('bindings')('segfault-handler-native');
+
