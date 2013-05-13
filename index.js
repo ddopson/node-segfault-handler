832d942a05a293a89a278c4ebc05b79711c8bdf1
Rolf Sommerhalder <rolf.sommerhalder@alumni.ethz.ch>
2013-05-13 12:47:44 +0200

    Make module load also under Node 0.10.x by using same module name throughout src/segfault-handler.cpp, binding.gyp, and index.js .

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 
 // The 'bindings' helper will try all the well-known paths that the module might compile to based on platform / version / debug / etc
-module.exports = require('bindings')('segfault-handler-native');
+module.exports = require('bindings')('segfault_handler');
 
